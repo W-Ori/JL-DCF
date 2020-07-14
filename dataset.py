a45d1a0404e5c02ab8065280a209379e2576acbf
WeiBinWu <1799164459@qq.com>
2020-07-14 21:00:10 +0800

    change1

--- dataset.py
+++ dataset.py
@@ -1,27 +1,22 @@
 import os
-<<<<<<< HEAD
+
 from PIL import Image
 import cv2
 import torch
 from torch.utils import data
 import numbers
 from torchvision.transforms import functional as F
-=======
 
 import cv2
 import torch
 from torch.utils import data
 
->>>>>>> eb3d1f5efe5940bb3b89c7acac3d4253b70568b1
 import numpy as np
 import random
 
 random.seed(10)
 
-<<<<<<< HEAD
-=======
 
->>>>>>> eb3d1f5efe5940bb3b89c7acac3d4253b70568b1
 class ImageDataTrain(data.Dataset):
     def __init__(self, data_root, data_list,image_size):
         self.sal_root = data_root
@@ -29,15 +24,14 @@ class ImageDataTrain(data.Dataset):
         self.image_size = image_size
 
         with open(self.sal_source, 'r') as f:
-<<<<<<< HEAD
+
             self.sal_list = [x.strip() for x in f.readlines()]
 
         self.sal_num = len(self.sal_list)
-=======
+
             self.sal_list = [x.strip() for x in f.readlines()]  
 
         self.sal_num = len(self.sal_list)  
->>>>>>> eb3d1f5efe5940bb3b89c7acac3d4253b70568b1
 
     def __getitem__(self, item):
         # sal data loading
@@ -49,19 +43,18 @@ class ImageDataTrain(data.Dataset):
         sal_label = load_sal_label(os.path.join(self.sal_root, gt_name), self.image_size)
 
         sal_image, sal_depth, sal_label = cv_random_crop(sal_image, sal_depth, sal_label, self.image_size)
-<<<<<<< HEAD
+
         sal_image = sal_image.transpose((2, 0, 1))
         sal_depth = sal_depth.transpose((2, 0, 1))
         sal_label = sal_label.transpose((2, 0, 1))
 
         sal_image = torch.Tensor(sal_image)
-=======
+
         sal_image = sal_image.transpose((2, 0, 1))  #矩阵转置
         sal_depth = sal_depth.transpose((2, 0, 1))
         sal_label = sal_label.transpose((2, 0, 1))
 
         sal_image = torch.Tensor(sal_image) # Tensor格式
->>>>>>> eb3d1f5efe5940bb3b89c7acac3d4253b70568b1
         sal_depth = torch.Tensor(sal_depth)
         sal_label = torch.Tensor(sal_label)
 
@@ -111,17 +104,17 @@ def get_loader(config, mode='train', pin=True):
 def load_image(path,image_size):
     if not os.path.exists(path):
         print('File {} not exists'.format(path))
-<<<<<<< HEAD
+
     im = cv2.imread(path)
     in_ = np.array(im, dtype=np.float32)
     in_ = cv2.resize(in_, (image_size, image_size))
     in_ = Normalization(in_)
-=======
+
     im = cv2.imread(path)    # 读入图片
     in_ = np.array(im, dtype=np.float32)  #转换为np格式
     in_ = cv2.resize(in_, (image_size, image_size))  # 图片缩放，（图片，大小）
     in_ = Normalization(in_)  # 正则化
->>>>>>> eb3d1f5efe5940bb3b89c7acac3d4253b70568b1
+
     return in_
 
 
@@ -131,17 +124,17 @@ def load_image_test(path,image_size):
         print('File {} not exists'.format(path))
     im = cv2.imread(path)
     in_ = np.array(im, dtype=np.float32)
-<<<<<<< HEAD
+
     im_size = tuple(in_.shape[:2])
     in_ = cv2.resize(in_, (image_size, image_size))
     in_ = Normalization(in_)
     in_ = in_.transpose((2, 0, 1))
-=======
+
     im_size = tuple(in_.shape[:2])   #shape:查看数组的维数
     in_ = cv2.resize(in_, (image_size, image_size))
     in_ = Normalization(in_)
     in_ = in_.transpose((2, 0, 1))  
->>>>>>> eb3d1f5efe5940bb3b89c7acac3d4253b70568b1
+
     return in_, im_size
 
 
@@ -171,7 +164,7 @@ def cv_random_crop(image, depth, label,image_size):
     label = label[..., np.newaxis]
     return image, depth, label
 
-<<<<<<< HEAD
+
 
 def cv_random_flip(image, depth, label):
     flip_flag = random.randint(0,1)
@@ -192,16 +185,13 @@ def cv_random_ratation(image, depth,label):
 
 
 
-
-=======
->>>>>>> eb3d1f5efe5940bb3b89c7acac3d4253b70568b1
 def Normalization(image):
     in_ = image[:, :, ::-1]
     in_ = in_ / 255.0
     in_ -= np.array((0.485, 0.456, 0.406))
     in_ /= np.array((0.229, 0.224, 0.225))
-<<<<<<< HEAD
+
     return in_
-=======
+
     return in_
 >>>>>>> eb3d1f5efe5940bb3b89c7acac3d4253b70568b1
